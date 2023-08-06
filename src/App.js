95b1f2d246840571578eb75a3f07477ee0a280eb
Done83 <nedomehan@gmail.com>
2023-08-06 15:06:37 +0200

    h1 deleted

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Navigation from "./components/Navigation";
 
 function App() {
     return <div className="App">
-        <h1>App component works!</h1>
         <Navigation />
         <Hero />
         <Projects />
