a307fefc3fbd3900fe6b91434142dacdc5fc87f0
Done83 <nedomehan@gmail.com>
2023-08-06 11:17:33 +0200

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./components/Navbar";
+import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -10,7 +10,7 @@ import Button from 'react-bootstrap/Button';
 function App() {
   return <div className="App">
     <h1>App component works!</h1>
-    <Navbar />
+    <Navigation />
     <Hero />
     <Projects />
     <Contact />
