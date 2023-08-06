c7c29c8e76f23af88d8f4fdd305a5166eaeced51
Done83 <nedomehan@gmail.com>
2023-08-06 14:24:11 +0200

    Add Hero/Carousel section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Button from 'react-bootstrap/Button';
+import Navigation from "./components/Navigation";
+
 
 function App() {
-  return <div className="App">
-    <h1>App component works!</h1>
-    <Navigation />
-    <Hero />
-    <Projects />
-    <Contact />
-    <Footer />
-    <Button variant="primary">Primary</Button>
-  </div>;
+    return <div className="App">
+        <h1>App component works!</h1>
+        <Navigation />
+        <Hero />
+        <Projects />
+        <Contact />
+        <Footer />
+        <Button variant="primary">Primary</Button>
+    </div>;
 }
 
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
 const Hero = () => {
-  return (
-    <div>
-Hero
-    </div>
-  )
+    return (
+        <Carousel data-bs-theme="dark">
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 max-vh-25"
+                    src="https://images.unsplash.com/photo-1616004655123-818cbd4b3143?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 max-vh-25"
+                    src="https://images.unsplash.com/photo-1566915896913-549d796d2166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 max-vh-25"
+                    src="https://images.unsplash.com/photo-1436262513933-a0b06755c784?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1742&q=80"
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                    <h5>Third slide label</h5>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
 }
 
-export default Hero;
+export default Hero
