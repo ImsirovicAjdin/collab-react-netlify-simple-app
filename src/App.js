6c69c247290a7d95c41bb06f28194ed0e3393865
ImsirovicAjdin <ajdika@live.com>
2023-08-06 10:50:27 +0200

    Add sample text to each component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return <div className="App">
     <h1>App component works!</h1>
+    <Navbar />
+    <Hero />
+    <Projects />
+    <Contact />
+    <Footer />
   </div>;
 }
 
