de0f7fe5f0b27075cc04d92cad16eb6b057c1fb9
ImsirovicAjdin <ajdika@live.com>
2023-08-06 11:03:22 +0200

    Add React-Bootstrap Button as test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return <div className="App">
@@ -13,6 +15,7 @@ function App() {
     <Projects />
     <Contact />
     <Footer />
+    <Button variant="primary">Primary</Button>
   </div>;
 }
 
