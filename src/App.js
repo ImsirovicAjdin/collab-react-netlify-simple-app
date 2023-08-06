efd58d5f1c6e55414e5f5b9a6ced42354a06cd19
Done83 <nedomehan@gmail.com>
2023-08-06 10:47:56 +0200

    Basic website structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
-import Footer from "./components/Footer";
 
 function App() {
   return <div className="App">
     <h1>App component works!</h1>
-    <Footer />
   </div>;
 }
 
